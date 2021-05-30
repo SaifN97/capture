b8c53ed00d1ccf79461e5272a270c4b369da33ee
SaifN97 <saifnarpali@gmail.com>
2021-05-30 19:52:53 +0530

    Added nav,Work page,routing and movie details page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
 // Global Styles
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 // Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// Router
+import { Switch, Route } from "react-router-dom";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
